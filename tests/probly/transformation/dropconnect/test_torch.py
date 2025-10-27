0540935fac73a13f0c9ef5ae1fd86c0605d64727
HaoLMU <pengyu.hao@campus.lmu.de>
2025-10-27 13:26:02 +0100

    add a simple test model

--- tests/probly/transformation/dropconnect/test_torch.py
+++ tests/probly/transformation/dropconnect/test_torch.py
@@ -1,2 +1,16 @@
 """Tests for probly.transformation.dropconnect.torch."""
 
+import pytest
+import torch
+from torch import nn
+
+
+@pytest.fixture
+def simple_model():
+    """A simple test model with two Linear Layers."""
+    return nn.Sequential(
+        nn.Linear(4,8),
+        nn.ReLU(),
+        nn.Linear(8,2),
+    )
+
